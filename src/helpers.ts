39ab154a4efe7f9a2c8c5d5fbcad956277f80af4
Joel Haker <joel@sourcetoad.com>
2021-11-02 10:44:14 -0400

    SIP-121 update: organize the helpers into alphabetical order

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,14 +1,14 @@
 const _hasOwnProperty = Object.prototype.hasOwnProperty;
 const _toString = Object.prototype.toString;
 
+export function hasOwnProperty(obj: unknown, key: string): boolean {
+    return _hasOwnProperty.call(obj, key);
+}
+
 /**
  * Strict object type check. Only returns true
  * for plain JavaScript objects.
  */
 export function isPlainObject(obj: unknown): boolean {
     return _toString.call(obj) === '[object Object]';
-}
-
-export function hasOwnProperty(obj: unknown, key: string): boolean {
-    return _hasOwnProperty.call(obj, key);
 }
